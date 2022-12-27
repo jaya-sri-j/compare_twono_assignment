8b68449183c84d09995a2379999195187dcd8372
jaya-sri-j <jayasrijagan28@gmail.com>
2022-12-27 10:24:34 +0530

    adding files.

---
+++ problem22-compare two numbers/js/main.js
@@ -0,0 +1,18 @@
+var n=prompt("enter the n value");
+n=parseInt(n);
+var m=prompt("enter the m value");
+m=parseInt(m);
+
+if(n==m)
+{
+alert(" true");
+}
+else if(n>m)
+{
+alert("true ");
+}
+else
+{
+alert("false");
+}
+
